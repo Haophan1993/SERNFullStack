9ad37e2741468db24ea0d975a0e3e54ca3049838
Hao Phan <haophan120193@gmail.com>
2024-10-05 16:22:04 -0400

    initial commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,29 @@
+import { json } from 'sequelize';
+import db from '../models/index';
+
+let getHomePage = async (req, res)=>{
+    try{
+        let data = await db.User.findAll();
+        console.log('------------------------')
+        console.log(data);
+        console.log('------------------------')
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        })
+    }catch(e){
+        console.log(e)
+    }
+
+    //return res.render('homepage.ejs')
+    
+   
+}
+
+let getAboutPage = (req,res)=>{
+    return res.render('about.ejs')
+}
+
+module.exports={
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage
+}
---
+++ src/seeders/20241005191558-demo-user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'new John',
+        lastName: 'New Doe',
+        email: 'newexample@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
